ec6123d1169f44855a3abebf73bf96fb9e5c9633
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-01-18 01:18:27 +0200

    Mobile remote UI working.

--- controller-ui/scripts.js
+++ controller-ui/scripts.js
@@ -1,4 +1,22 @@
-var serverUrl = "http://localhost:3000/control";
+var serverUrl = "http://192.168.2.18:3000/control";
+var eventsToListen = "tap";
+
+
+var forwardBtn = new Hammer(document.getElementById('forwardBtn'));
+forwardBtn.on(eventsToListen, function(ev) {
+    ev.preventDefault();
+    goForward();
+});
+var leftBtn = new Hammer(document.getElementById('leftBtn'));
+leftBtn.on(eventsToListen, function(ev) {
+    ev.preventDefault();
+    goLeft();
+});
+var rightBtn = new Hammer(document.getElementById('rightBtn'));
+rightBtn.on(eventsToListen, function(ev) {
+    ev.preventDefault();
+    goRight();
+});
 
 function goForward(){
     console.log("Forward");
