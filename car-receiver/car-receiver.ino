c52e977103d7a180d31a008df3d84bcf54a0b68e
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-01-17 17:34:43 +0200

    Simple direction control.

--- car-receiver/car-receiver.ino
+++ car-receiver/car-receiver.ino
@@ -4,10 +4,15 @@
 
 #include <VirtualWire.h>
 
+int motorL = 12;
+int motorR = 8;
+int ledPin = 13;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as common motor control TODO
-  pinMode(13, OUTPUT);
+  pinMode(motorR, OUTPUT);
+  pinMode(motorL, OUTPUT);
 
   // Radio receiver
   Serial.begin(9600);
@@ -17,20 +22,59 @@ void setup() {
 }
 
 // the loop function runs over and over again forever
-void loop() {
+int controlVal;
 
+void motor_controller(char controlChar){
+  controlVal = controlChar - 48; // TODO fix this properly
+
+  Serial.print("Got: ");
+  Serial.println(String(controlVal));
+
+  switch (controlVal){
+  case 0:
+    digitalWrite(motorL, HIGH);
+    digitalWrite(motorR, HIGH);
+    Serial.println("Forward");
+
+    break;
+  case 1:
+    digitalWrite(motorR, HIGH);
+    Serial.println("Left");
+
+    break;
+  case 2:
+    digitalWrite(motorL, HIGH);
+    Serial.println("Right");
+
+    break;
+  default:
+    digitalWrite(ledPin, HIGH);
+    Serial.println("ERROR: Odd value!");
+  }
+
+  delay(300);
+
+  digitalWrite(motorL, LOW);
+  digitalWrite(motorR, LOW);
+  digitalWrite(13, LOW);
+}
+
+void loop() {
   byte message[VW_MAX_MESSAGE_LEN];
   byte messageLength = VW_MAX_MESSAGE_LEN;
 
-
   if (vw_get_message(message, &messageLength)){
-    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-    Serial.print("Received: ");
     for (int i = 0; i < messageLength; i++){
-      Serial.write(message[i]);
-      delay(10);
+      //Serial.write(message[i]);
+      motor_controller(message[i]);
+      //delay(10);
     }
-    Serial.println();
-    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   }
+
+  else {
+    digitalWrite(ledPin, HIGH);
+    delay(50);
+    digitalWrite(ledPin, LOW);
+  }
+
 }
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,6 +1,6 @@
 /*
   by Jarkko Saltiola (jasalt)
- */
+*/
 
 #include <VirtualWire.h>
 
@@ -8,7 +8,11 @@ void setup(){
   vw_setup(2000); // bps
 }
 void loop(){
-  send("hello world world wolrd"); //your message
+  send("1"); // left
+  delay(1000);
+  send("2"); // right
+  delay(1000);
+  send("0"); // forward
   delay(1000);
 }
 void send (char *message){
