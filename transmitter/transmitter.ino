20181ce6c247cca27847ebbe6ec0c93b9f1ac43e
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-01-17 18:54:34 +0200

    Transmitter listens for serial.

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -4,16 +4,35 @@
 
 #include <VirtualWire.h>
 
+void serial_setup(){
+  Serial.begin(9600);
+}
+
 void setup(){
   vw_setup(2000); // bps
+  serial_setup();
 }
-void loop(){
+
+void test_loop(){
   send("1"); // left
   delay(1000);
   send("2"); // right
   delay(1000);
   send("0"); // forward
   delay(1000);
+}
+
+char serialChar;
+void serial_control(){
+  while (Serial.available() > 0) {
+    serialChar = Serial.read();
+    send(&serialChar);
+  }
+}
+
+void loop(){
+  //test_loop();
+  serial_control();
 }
 void send (char *message){
   vw_send((uint8_t *)message, strlen(message));
